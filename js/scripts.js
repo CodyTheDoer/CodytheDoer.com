941a2a9e3232cceeea0ae893e159b60a9d73366f
CodyTheDoer <me@codythedoer.com>
2020-07-10 16:34:26 +0000

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-function floatTank(){
+function floatTank(float){
     const floatTankContainer = document.querySelector('div#floatTank');
     const floats = document.getElementsByClassName('float');
+
+    if(float === home){
+        hideFloats();
+        floats[0].style.display = "block";
+    };
+    if(float === services){};
+    if(float === resume){};
+    if(float === contact){};
 }
+
+function hideFloats(){
+    const floats = document.getElementsByClassName('float');
+    for(i=0; i < floats.length; i++){
+        floats[i].style.display = "none";
+    }
+}
+
+window.onload = () => {
+    hideFloats();
+};
