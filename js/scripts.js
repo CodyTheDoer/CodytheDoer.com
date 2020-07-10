db936630b3d4fa6a4e531ef4674c0b40d022bdbb
CodyTheDoer <me@codythedoer.com>
2020-07-10 16:40:52 +0000

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,27 @@ function floatTank(float){
     const floatTankContainer = document.querySelector('div#floatTank');
     const floats = document.getElementsByClassName('float');
 
-    if(float === home){
+    if(float === "home"){
         hideFloats();
         floats[0].style.display = "block";
+        floats[1].style.display = "block";
     };
-    if(float === services){};
-    if(float === resume){};
-    if(float === contact){};
+    if(float === "contact"){
+        hideFloats();
+        floats[2].style.display = "block";
+    };
+    if(float === "services"){
+        hideFloats();
+        floats[3].style.display = "block";
+        floats[4].style.display = "block";
+        floats[5].style.display = "block";
+        floats[6].style.display = "block";
+        floats[7].style.display = "block";
+    };
+    if(float === "resume"){
+        hideFloats();
+        floats[8].style.display = "block";
+    };
 }
 
 function hideFloats(){
