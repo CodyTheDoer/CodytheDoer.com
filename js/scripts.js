50541c1964f4f763edaf9a11881816f8a22a5ede
CodyTheDoer <me@codythedoer.com>
2020-07-11 00:25:49 +0000

    Cleaned up content and added in float-headers for easy CSS maintenance.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
 function floatTank(float){
-    const floatTankContainer = document.querySelector('div#floatTank');
     const floats = document.getElementsByClassName('float');
 
     if(float === "home"){
         hideFloats();
         floats[0].style.display = "block";
-        floats[1].style.display = "block";
+        floats[8].style.display = "block";
     };
     if(float === "contact"){
         hideFloats();
-        floats[2].style.display = "block";
+        floats[1].style.display = "block";
     };
     if(float === "services"){
         hideFloats();
+        floats[2].style.display = "block";
         floats[3].style.display = "block";
         floats[4].style.display = "block";
         floats[5].style.display = "block";
-        floats[6].style.display = "block";
+        floats[8].style.display = "block";
     };
     if(float === "resume"){
         hideFloats();
-        floats[7].style.display = "block";
+        floats[6].style.display = "block";
+        floats[8].style.display = "block";
     };
     if(float === "projects"){
         hideFloats();
+        floats[7].style.display = "block";
         floats[8].style.display = "block";
     };
 }
